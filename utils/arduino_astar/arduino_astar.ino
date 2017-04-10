7ec9eba702f2571f4c4d2337e7466d67f7018546
jefersonla <oi_je@hotmail.com>
2017-04-09 23:50:59 -0300

    Used ctrl+t to beautify (auto adjust) identation

--- utils/arduino_astar/arduino_astar.ino
+++ utils/arduino_astar/arduino_astar.ino
@@ -18,8 +18,8 @@ int heuristic[NUM_LINHAS][NUM_COLUNAS];
 
 
 /* Váriaveis para sinalizar fim do programa */
-int fim = 0;           
-int counter = 0;       
+int fim = 0;
+int counter = 0;
 
 /* Define objetivo*/
 int GoalRow = 1;
@@ -40,9 +40,9 @@ int StartCol = 1;
 /* Váriaveis utilizadas na função de custo */
 
 /* Configura custo em andar pelas ortogonais */
-int ort = 10;  
+int ort = 10;
 /* Configura custo em andar pelas diagonais */
-int obstacle = 1000; 
+int obstacle = 1000;
 
 /* Variáveis auxiliares */
 int i;
@@ -60,44 +60,44 @@ int finalRow[m];
 int finalCol[m];
 
 /* Função usada no teste, para finalizar o programa após uma execução */
-void intervencao(){
-  for(i = 0; i < m; i++){
-      for(j = 0; j < m; j++){
-         Serial.println(grid[i][j]);
-         fim = 1;
-      }
-   }
+void intervencao() {
+  for (i = 0; i < m; i++) {
+    for (j = 0; j < m; j++) {
+      Serial.println(grid[i][j]);
+      fim = 1;
+    }
+  }
 }
 
 void setup() {
-   Serial.begin(115200);
-   Serial.println("Estou no Setup!");
-   
-   /* Inicializa minha matriz com as distâncias de Manhattan, função heurística */
-   heuristicFunction();
-   ActualRow = StartRow;
-   ActualCol = StartCol;
-   /* O caminho obviamente inicia pelo ponto inicial */
-   pathRow[0] = StartRow;
-   pathCol[0] = StartCol;
-   z = 1;
+  Serial.begin(115200);
+  Serial.println("Estou no Setup!");
 
-   /* Setando todo o grid para 1000, isso irá auxiliar no momento da execução do A* */
-   for(i = 0; i < m; i++){
-      for(j = 0; j < m; j++){
-         grid[i][j] = INT_MAX;
-      }
-   }
-   
-   /* Local das funções de ajustes de rodas */
-   //calibratemotors()
+  /* Inicializa minha matriz com as distâncias de Manhattan, função heurística */
+  heuristicFunction();
+  ActualRow = StartRow;
+  ActualCol = StartCol;
+  /* O caminho obviamente inicia pelo ponto inicial */
+  pathRow[0] = StartRow;
+  pathCol[0] = StartCol;
+  z = 1;
+
+  /* Setando todo o grid para 1000, isso irá auxiliar no momento da execução do A* */
+  for (i = 0; i < m; i++) {
+    for (j = 0; j < m; j++) {
+      grid[i][j] = INT_MAX;
+    }
+  }
+
+  /* Local das funções de ajustes de rodas */
+  //calibratemotors()
 }
 
 void loop() {
   Serial.println("Inicio do loop!");
   delay(1000);
-  if(!fim){
-  	/* Giro o robo, enquanto calculo o A* para cada quadrado observado */
+  if (!fim) {
+    /* Giro o robo, enquanto calculo o A* para cada quadrado observado */
     a_star();
     delay(1000);
     /* Encontra o menor valor no Grid para definir próxima posição */
@@ -112,27 +112,27 @@ void loop() {
     printheuristic();
     delay(1000);
     /* Caso seja o fim, finalize e imprima o resultado da computaçao */
-    if(isFim()){
-       /* Imprime caminho final */
-       defineWay();
-       delay(10000);
+    if (isFim()) {
+      /* Imprime caminho final */
+      defineWay();
+      delay(10000);
     }
     delay(2000);
     /* Descomente a linha a seguir para parar o código após uma interação */
     /* intervencao(); */
   }
-  
-  else{
-  /* Robo reexecuta o movimento correto ao apertar o botão */
-  /* TODO */
+
+  else {
+    /* Robo reexecuta o movimento correto ao apertar o botão */
+    /* TODO */
   }
   Serial.println("Fim do loop!");
 }
 
 /* Printa o caminho percorrido*/
-void printpath(){
+void printpath() {
   Serial.println("Este eh ao caminho percorrido ate aqui:");
-  for(i = 0; i < z; i++){
+  for (i = 0; i < z; i++) {
     Serial.print(pathRow[i]);
     Serial.print(" ");
     Serial.println(pathCol[i]);
@@ -140,39 +140,39 @@ void printpath(){
 }
 
 /* Printa a matriz Heuristica*/
-void printheuristic(){
+void printheuristic() {
   Serial.println("Este eh a heuristica do labirinto, resolvido por Manhanttan:");
-  for(i = 0; i < NUM_LINHAS; i++){
-    for(j = 0; j < NUM_LINHAS; j++){
+  for (i = 0; i < NUM_LINHAS; i++) {
+    for (j = 0; j < NUM_LINHAS; j++) {
       Serial.print(heuristic[i][j]);
       Serial.print("  ");
     }
     Serial.print("\n");
-  }  
+  }
 }
 
 /* Printa o grid */
-void printgrid(){
+void printgrid() {
   Serial.println("Este eh o grid do labirinto, resolvido por A*:");
-  for(i = 0; i < m; i++){
-    for(j = 0; j < m; j++){
+  for (i = 0; i < m; i++) {
+    for (j = 0; j < m; j++) {
       Serial.print(grid[i][j]);
       Serial.print("  ");
     }
     Serial.print("\n");
-  }  
+  }
 }
 
 /* Verifica se é o fim */
-boolean isFim(){
+boolean isFim() {
   Serial.println("Verificando se eh o fim...");
   //This funcion will see if we reach the goal
   //if ActualPoint is equal GoalPoint
-  if(ActualRow == GoalRow && ActualCol == GoalCol){
+  if (ActualRow == GoalRow && ActualCol == GoalCol) {
     fim = 1;
-    return true; 
+    return true;
   }
-  else{
+  else {
     return false;
   }
 }
@@ -180,13 +180,13 @@ boolean isFim(){
 
 /* Detecta obstaculo */
 
-boolean isObstacle(int x, int y){
+boolean isObstacle(int x, int y) {
   Serial.println("Verificando obstaculos...");
   /* Barreiras virtualmente colocadas */
-  if (x == 0 or y == 0 or x == 4 or y == 4){
+  if (x == 0 or y == 0 or x == 4 or y == 4) {
     return true;
   }
-  else{
+  else {
     return false;
   }
 }
@@ -194,31 +194,31 @@ boolean isObstacle(int x, int y){
 /* Movimentos do robo */
 
 /* Move para a frente */
-void goForward(){
+void goForward() {
   Serial.println("Movendo para frente...");
   /* TODO */
 }
 
 /* Move para trás */
-void goBackward(){
+void goBackward() {
   Serial.println("Movendo para tras...");
   /* TODO */
 }
 
 /* Move para esquerda */
-void turnLeft(){
+void turnLeft() {
   Serial.println("Movendo para esquerda...");
   /* TODO */
 }
 
 /* Move para direita */
-void turnRight(){
+void turnRight() {
   Serial.println("Movendo para direita...");
   /* TODO */
 }
 
 /* Gira o robô para a direita */
-void turnAround(){
+void turnAround() {
   Serial.println("Girando...");
   /* TODO */
 }
@@ -230,111 +230,111 @@ int aux1;
 int aux2;
 
 /* Funcao Heuristica calculada com a distância de Manhanttan */
-void heuristicFunction(){
+void heuristicFunction() {
   Serial.println("Calculando matriz heuristica...");
-  for(i = 0; i < NUM_LINHAS; i++){
-    for(j = 0; j < NUM_LINHAS; j++){
-      aux1 = abs(i - (GoalRow-1));
-      aux2 = abs(j - (GoalCol-1));
+  for (i = 0; i < NUM_LINHAS; i++) {
+    for (j = 0; j < NUM_LINHAS; j++) {
+      aux1 = abs(i - (GoalRow - 1));
+      aux2 = abs(j - (GoalCol - 1));
       aux1 = aux1 + aux2;
       heuristic[i][j] = aux1;
       /*
-      Serial.print("heuristic[");
-      Serial.print(i);
-      Serial.print("][");
-      Serial.print(j);
-      Serial.print("] = ");
-      Serial.println(heuristic[i][j]);
+        Serial.print("heuristic[");
+        Serial.print(i);
+        Serial.print("][");
+        Serial.print(j);
+        Serial.print("] = ");
+        Serial.println(heuristic[i][j]);
       */
     }
-  }  
+  }
 }
 
 /* Funcao A Estrela (Junta funcao Heuristica com as variáveis de Custo) */
-void a_star(){
+void a_star() {
   Serial.println("Aplicando A* em meus vizinhos...");
 
   /* Para evitar recalculos, seta o quadrado atual como 2000*/
   grid[ActualRow][ActualCol] = 2000;
-   
+
   /* Calculo o A Estrela do quadrado da frente */
-  setGrid(ActualRow-1,ActualCol);
+  setGrid(ActualRow - 1, ActualCol);
   /* Depois giro para a direita */
   turnAround();
   /* Calculo o A Estrela do quadrado da direita */
-  setGrid(ActualRow,ActualCol+1);
+  setGrid(ActualRow, ActualCol + 1);
   /* Depois giro para a direita */
   turnAround();
   /* Calculo o A Estrela do quadrado de trás */
-  setGrid(ActualRow+1,ActualCol);
+  setGrid(ActualRow + 1, ActualCol);
   /* Depois giro para a direita */
   turnAround();
   /* Calculo o A Estrela do quadrado da esquerda */
-  setGrid(ActualRow,ActualCol-1);
+  setGrid(ActualRow, ActualCol - 1);
   /* Depois giro para a direita, para voltar a orientação inicial */
   turnAround();
 }
 
 /* Percorro minha matriz Grid em busca do menor valor, o primeiro menor valor será minha próxima posição */
-void findPath(){
+void findPath() {
   Serial.println("Definindo rota...");
   p = z - 1;
   int padrao = 1000;
-  for(i = 1; i < m; i++){
-    for(j = 1; j < m; j++){
-       if(grid[i][j] < padrao){
+  for (i = 1; i < m; i++) {
+    for (j = 1; j < m; j++) {
+      if (grid[i][j] < padrao) {
         NextRow = i;
         NextCol = j;
         /*Serial.print(NextRow);
-        Serial.println(NextCol);*/
+          Serial.println(NextCol);*/
         padrao = grid[i][j];
-       }
+      }
     }
   }
 }
 
 /* Define o novo valor do quadrado observado (até então, desconhecido) */
-void setGrid(int x, int y){
+void setGrid(int x, int y) {
   Serial.println("Calculando heuristica mais custo...");
   int q = x;
   int r = y;
   /* Se for obstaculo, valor 1000 */
-  if (isObstacle(q, r)){
-    if (q >= 0 && r >= 0){
+  if (isObstacle(q, r)) {
+    if (q >= 0 && r >= 0) {
       grid[x][y] = obstacle;
       return;
     }
   }
   /* Se já foi calculado, ignora */
-  if (grid[x][y] == 2000){ //(grid[x][y] == 2000)
-      return;
+  if (grid[x][y] == 2000) { //(grid[x][y] == 2000)
+    return;
   }
   /* Calculo custo do caminho até chegar mais a heuritica */
-  else{
-      grid[x][y] = 0;
-      grid[x][y] = 10 + heuristic[x-1][y-1];
-     Serial.print("Este eh meu indice Z: ");
-     Serial.println(z);
-     p = z-1;
-     int lock2 = 1;
-     printpath();
-     while(lock2){
-      if(pathRow[p] == StartRow && pathCol[p] == StartCol){
-        lock2 = 0;  
+  else {
+    grid[x][y] = 0;
+    grid[x][y] = 10 + heuristic[x - 1][y - 1];
+    Serial.print("Este eh meu indice Z: ");
+    Serial.println(z);
+    p = z - 1;
+    int lock2 = 1;
+    printpath();
+    while (lock2) {
+      if (pathRow[p] == StartRow && pathCol[p] == StartCol) {
+        lock2 = 0;
       }
-      else{  
+      else {
         /*Serial.print(pathRow[p]);
-        Serial.print("  ");
-        Serial.println(pathCol[p]);*/
-        grid[x][y] += heuristic[pathRow[p]-1][pathCol[p]-1];
-        p-=1;
-      }  
+          Serial.print("  ");
+          Serial.println(pathCol[p]);*/
+        grid[x][y] += heuristic[pathRow[p] - 1][pathCol[p] - 1];
+        p -= 1;
+      }
     }
   }
 }
 
 /* Encontra um caminho até a próxima posição */
-void goAhead(){
+void goAhead() {
   Serial.println("Indo para proxima posicao...");
   Serial.print("Linha: ");
   Serial.print(NextRow);
@@ -342,33 +342,33 @@ void goAhead(){
   Serial.println(NextCol);
   //Verifico se próxima posição é um vizinho
   /* Se for,  eles estão na mesma linha, devo ir para direita ou esquerda */
-  if(ActualRow == NextRow){  
-    while(ActualCol != NextCol){   
-      if(ActualCol < NextCol){
+  if (ActualRow == NextRow) {
+    while (ActualCol != NextCol) {
+      if (ActualCol < NextCol) {
         turnRight();
         ActualRow = NextRow;
-        ActualCol +=1;
+        ActualCol += 1;
       }
-      else{
-        turnLeft(); 
+      else {
+        turnLeft();
         ActualRow = NextRow;
         ActualCol -= 1;
       }
       pathRow[z] = ActualRow;
       pathCol[z] = ActualCol;
       z += 1;
-    }  
+    }
     return;
   }
   /* Se for,  eles estão na mesma linha, devo ir para frente ou trás */
-  if(ActualCol == NextCol){  
-     while(ActualRow != NextRow){ 
-      if(ActualRow < NextRow){
+  if (ActualCol == NextCol) {
+    while (ActualRow != NextRow) {
+      if (ActualRow < NextRow) {
         goBackward();
         ActualRow += 1;
         ActualCol = NextCol;
       }
-      else{
+      else {
         goForward();
         ActualRow -= 1;
         ActualCol = NextCol;
@@ -376,66 +376,66 @@ void goAhead(){
       pathRow[z] = ActualRow;
       pathCol[z] = ActualCol;
       z += 1;
-    } 
+    }
     return;
   }
   /* Caso não seja vizinho, devo regredir no grid, retrocendendo na lista percorrida até alcançar um vizinho */
   Serial.println("Nao e vizinho.............................");
   p -= 1;
   /* Se for,  eles estão na mesma linha, devo ir para direita ou esquerda */
-  if(ActualRow == pathRow[p]){ 
-     if(ActualCol < pathCol[p]){
+  if (ActualRow == pathRow[p]) {
+    if (ActualCol < pathCol[p]) {
       turnRight();
-     }
-     else{
-      turnLeft(); 
-     }
-     ActualRow = pathRow[p];
-     ActualCol = pathCol[p];
-     pathRow[z] = ActualRow;
-     pathCol[z] = ActualCol;
-     z += 1;
-     goAhead();
+    }
+    else {
+      turnLeft();
+    }
+    ActualRow = pathRow[p];
+    ActualCol = pathCol[p];
+    pathRow[z] = ActualRow;
+    pathCol[z] = ActualCol;
+    z += 1;
+    goAhead();
   }
   /* Se for,  eles estão na mesma linha, devo ir para frente ou trás */
-  if(ActualCol == pathCol[p]){  
-     if(ActualRow < pathRow[p]){
+  if (ActualCol == pathCol[p]) {
+    if (ActualRow < pathRow[p]) {
       goBackward();
-     }
-     else{
+    }
+    else {
       goForward();
-     }
-     ActualRow = pathRow[p];
-     ActualCol = pathCol[p];
-     pathRow[z] = ActualRow;
-     pathCol[z] = ActualCol;
-     z += 1;
-     goAhead();
+    }
+    ActualRow = pathRow[p];
+    ActualCol = pathCol[p];
+    pathRow[z] = ActualRow;
+    pathCol[z] = ActualCol;
+    z += 1;
+    goAhead();
   }
-  goAhead(); 
+  goAhead();
 }
 
 
-void defineWay(){
+void defineWay() {
   p = z;
   int lock = 1;
   Serial.println("Imprimindo caminho final...");
   /* Retrocede o indice da lista até encontrar a posição inicial */
-  while(lock){
-    if(pathRow[p] == StartRow && pathCol[p] == StartCol){
-      lock = 0;  
+  while (lock) {
+    if (pathRow[p] == StartRow && pathCol[p] == StartCol) {
+      lock = 0;
     }
-    else{  
+    else {
       counter++;
       p--;
-    }  
+    }
   }
   /* Imprime caminho final */
-  for(i = 0; i < counter; i++){
+  for (i = 0; i < counter; i++) {
     finalRow[i] = pathRow[p];
     finalCol[i] = pathCol[p];
     Serial.print("Passo ");
-    Serial.print(i+1);
+    Serial.print(i + 1);
     Serial.print(" ");
     Serial.print(pathRow[p]);
     Serial.print(" ");
